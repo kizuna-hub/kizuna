6f55f18744c7731f1b165af99356bd08659e17d9
sangoonthego <knkidngoc@gmail.com>
2026-07-30 00:58:46 +0700

    refactor: remove remaining legacy founder dashboard modules

--- features/founder/founder-dashboard/home-feed-screen.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { FounderDashboard } from "@/features/founder/founder-dashboard/platform/overview";
-
-export function HomeFeedScreen() {
-  return <FounderDashboard />;
-}
